dd9354c2b3c70625639f587ed6d7a63fa786f8b5
ham-maus <hamutaro86@outlook.com>
2026-02-01 19:50:30 +0900

    tps カメラ作った

--- Assets/camerascript.cs
+++ Assets/camerascript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class camerascript : MonoBehaviour
 {
+    public Transform player;
+    public float cameray = 5;
+    public float cameray2 = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,12 @@ public class camerascript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        cameray += Input.GetAxis("Mouse Y");
+        cameray2 = -cameray;
+        cameray2 = Mathf.Clamp(cameray2, -60, 60);
+
+        Vector3 offset = -player.forward * 10 + Vector3.up * 5;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position + Vector3.up * 2 + Vector3.down * cameray2); ;
     }
 }
